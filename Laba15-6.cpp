acdbb70121b6341ea6b02a2d8ac060661c5d7c56
MaksGrigorenko123 <148767163+MaksGrigorenko123@users.noreply.github.com>
2023-12-14 21:25:21 +0200

    Add files via upload

---
+++ Laba15-6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>//підключаємо директиву вводу та виводу 
+#define SIZE 3//зазначаємо константу 
+using namespace std;//використовуємо простір імен std
+
+int main() {
+    int numbers[SIZE];//створюємо масив 
+
+    cout << "Enter 3 numbers: ";//вивід повідомлення 
+    for (int i = 0; i < SIZE; ++i) {//цикл для введення даних 
+        cin >> numbers[i];//введеня даних 
+    }
+
+    int minValue = numbers[0];//задання змінної
+    int maxValue = numbers[0];//задання змінної 
+
+    for (int i = 1; i < SIZE; ++i) {//цикл для перевірки значень 
+        if (numbers[i] < minValue) {//Логічна умова 
+            minValue = numbers[i];
+        }//Умова якщо виконується  
+        if (numbers[i] > maxValue) {//Логічна умова 
+            maxValue = numbers[i];
+        }//Умова якщо виконується
+    }
+
+
+    cout << "Minimum value: " << minValue << " Maximum value: " << maxValue;// Виведення результату
+
+    return 0;//Логічний кінець 
+}
